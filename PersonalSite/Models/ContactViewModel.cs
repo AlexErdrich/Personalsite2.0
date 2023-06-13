82e87a5bc7024310b300763b752d0cb6275f48b5
AlexErdrich <erdrich612@hotmail.com>
2023-06-13 09:06:06 -0500

    Site

---
+++ PersonalSite/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PersonalSite.Controllers;
+
+namespace PersonalSite.Models
+{
+    public class ContactViewModel
+    {
+        //We can use DataAnnotations to add validation to our model.
+        //This is Useful when we have required fields or need certain types of  info. 
+        [Required(ErrorMessage = "* Required")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.EmailAddress)] //enforces standard email formatting
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        public string Subject { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.MultilineText)]// Gives us a larger text (textarea)
+        public string Message { get; set; } = null!;
+
+    }
+}
