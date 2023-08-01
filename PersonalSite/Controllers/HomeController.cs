514d6d88347954c7f93f29e8973b5539262b3754
AlexErdrich <erdrich612@hotmail.com>
2023-08-01 12:39:46 -0500

    Email functionaility

--- PersonalSite/Controllers/HomeController.cs
+++ PersonalSite/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PersonalSite.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+
 
 namespace PersonalSite.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
@@ -22,16 +27,69 @@ namespace PersonalSite.Controllers
         {
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            return View(); 
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       
+        
+        public IActionResult Contact()
+        {
+            return View(); 
+        }      
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm)
+        {            
+            if (!ModelState.IsValid)
+            {                
+                return View(cvm);
+            }                       
+            string message = $"You have received a new email from your site's contact form!<br>" +
+                $"Sender: {cvm.Name}<br>" +
+                $"Email: {cvm.Email}<br>" +
+                $"Message: {cvm.Message}";                       
+            var mm = new MimeMessage();
+            
+            mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+
+            mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+            mm.Subject = cvm.Subject;
+
+            mm.Body = new TextPart("HTML") { Text = message };
+          
+            mm.Priority = MessagePriority.Urgent;
+            
+            using (var client = new SmtpClient())
+            {
+                
+                client.Connect(_config.GetValue<string>("Credentials:Email:Client"), 8889);
+
+                    client.Authenticate(
+                    
+                        _config.GetValue<string>("Credentials:Email:User"),
+                   
+                        _config.GetValue<string>("Credentials:Email:Password")
+                        );
+                try
+                {                    
+                    //client.Connect(_config.GetValue<string>("Credentials:Email:Client"), 8889);
+                    
+                    
+                    client.Send(mm);
+                }               
+                catch (Exception ex)
+                {
+                    
+                    ViewBag.ErrorMessage = $"There was an error processing your request. Please try again later.<br>Error Message: {ex.Message}";
+
+                    return View(cvm);
+                }
+            }
+            return View("EmailConfirmation", cvm);
         }
     }
 }
--- PersonalSite/Models/ContactViewModel.cs
+++ PersonalSite/Models/ContactViewModel.cs
@@ -1,24 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using PersonalSite.Controllers;
 
+
 namespace PersonalSite.Models
 {
     public class ContactViewModel
     {
-        //We can use DataAnnotations to add validation to our model.
-        //This is Useful when we have required fields or need certain types of  info. 
+        
         [Required(ErrorMessage = "* Required")]
         public string Name { get; set; } = null!;
 
         [Required(ErrorMessage = "* Required")]
-        [DataType(DataType.EmailAddress)] //enforces standard email formatting
+        [DataType(DataType.EmailAddress)] 
         public string Email { get; set; } = null!;
 
         [Required(ErrorMessage = "* Required")]
         public string Subject { get; set; } = null!;
 
         [Required(ErrorMessage = "* Required")]
-        [DataType(DataType.MultilineText)]// Gives us a larger text (textarea)
+        [DataType(DataType.MultilineText)]
         public string Message { get; set; } = null!;
 
     }
